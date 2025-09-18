e1e36199aa83a1ccf82b29251930d7577ce1a520
Labs <117577809+devops-success-true@users.noreply.github.com>
2025-09-18 14:59:54 +0200

    Create main.tf

---
+++ terraform/addons/prometheus/main.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "prometheus" {
+  name       = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = "monitoring"
+  create_namespace = true
+
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+}
