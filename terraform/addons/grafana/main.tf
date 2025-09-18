f8a5ac59c117a193b5d270fe994b706d7e43649e
Labs <117577809+devops-success-true@users.noreply.github.com>
2025-09-18 15:01:31 +0200

    Create main.tf

---
+++ terraform/addons/grafana/main.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  namespace  = "monitoring"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  version    = "6.57.4"
+
+  values = [file("${path.module}/values.yaml")]
+}
